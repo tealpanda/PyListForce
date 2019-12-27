6ef46c019da7f71213517bb678ee74da8e183154
Frederic Crespo Arredondo <crespo.frederic@gmail.com>
2019-12-27 11:47:11 +0100

    file ouput
    Added saving output to file and gitignore

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ def progress(count, total, status=''):
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
 
+def write_line(file, line):
+    with open(file, 'a') as opened_file:
+        opened_file.write(line + '\n')
+
 parser = argparse.ArgumentParser(description='Scans the target URL for potential hidden paths.')
 
 parser.add_argument('target', help="The target URL to scan.")
 parser.add_argument('--dict', default="all", help="The dictionary to use for scanning.")
+parser.add_argument('--file', default="output.txt", help="The file to write scan results to.")
 args = vars(parser.parse_args())
 
 #target = "http://34.94.3.143/23f816a954/" # To be used as input
@@ -31,15 +36,11 @@ for word in dictionary:
     try:
         response = requests.get(url)
     except:
-        pass
-        #print(url)
-        #print('Exception')
+        write_line(args['file'], url + ',Exception')
 
     if (response):
         if (response.status_code != 404):
-            pass
-            #print(url)
-            #print(response.status_code)
+            write_line(args['file'], url + ',' + str(response.status_code))
 
     count += 1
     progress(count, len(dictionary), status='scanned')
